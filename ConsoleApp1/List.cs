d2c60f704a623c0289e7b83505f81f8b09f0556b
Natalya <NatalyaDemiD7@gmail.com>
2019-05-28 20:37:51 +0300

    Init commit

---
+++ ConsoleApp1/List.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    interface IDynamicArr<T>
+    {
+        void Add(T item);
+        void Insert(int index, T item);
+        void Remove(T item);
+        void RemoveAt(int index);
+        void Clear();
+        bool Contains(T item);
+        int IndexOf(T item);
+        T[] ToArray();
+        void Reverse();
+    }
+
+    class List<T> : IDynamicArr<T>
+    {
+        private T[] inner;
+
+        private int сount;
+        public int Count
+        {
+            get
+            {
+                return сount;
+            }
+            private set
+            {
+                if (value > 0)
+                {
+                    сount = value;
+                }
+            }
+        }
+
+        //индексатор
+        public T this[int index]
+        {
+            get
+            {
+                return inner[index];
+            }
+            set
+            {
+                inner[index] = value;
+            }
+        }
+
+
+        public void Add(T item)
+        {
+            if (inner == null)
+            {
+                inner = new T[] { item };
+            }
+            else
+            {
+                T[] newinner = new T[inner.Length + 1];
+
+                int i = 0;
+                while (i != (newinner.Length - 1))
+                {
+                    newinner[i] = inner[i];
+                    i++;
+                }
+
+                newinner[newinner.Length - 1] = item;
+
+                inner = newinner;
+            }
+
+            Count++;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (inner == null || inner.Length < index || index < 0)
+            {
+                Console.WriteLine("Ошибка");
+            }
+            else
+            {
+                T[] newinner = new T[inner.Length + 1];
+
+                int i = 0;
+                while (i != index - 1)
+                {
+                    newinner[i] = inner[i];
+                    i++;
+                }
+
+                newinner[index - 1] = item;
+
+                i = index - 1;
+                while (i < newinner.Length - 1)
+                {
+                    newinner[i + 1] = inner[i];
+                    i++;
+                }
+
+                inner = newinner;
+            }
+        }
+
+        public void Remove(T item)
+        {
+            if (IndexOf(item) != -1)
+            {
+                RemoveAt(IndexOf(item));
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            T[] newinner = new T[inner.Length - 1];
+
+            int i = 0;
+            while (i != index)
+            {
+                newinner[i] = inner[i];
+                i++;
+            }
+
+            i = index;
+            while (i < newinner.Length)
+            {
+                newinner[i] = inner[i + 1];
+                i++;
+            }
+
+            inner = newinner;
+        }
+
+        public int IndexOf(T item)
+        {
+            int index = -1;
+
+            for (int j = 0; j < inner.Length; j++)
+            {
+                if (inner[j].Equals(item))
+                {
+                    index = j;
+                    j = inner.Length;
+                }
+            }
+
+            return index;
+        }
+
+        public void Clear()
+        {
+            T[] newinner = new T[0];
+
+            inner = newinner;
+        }
+
+        public bool Contains(T item)
+        {
+            bool result = false;
+
+            for (int i = 0; i < inner.Length; i++)
+            {
+                if (inner[i].Equals(item))
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        public T[] ToArray()
+        {
+            T[] result = new T[this.Count];
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                result[i] = inner[i];
+            }
+
+            return result;
+        }
+
+        public void Reverse()
+        {
+            T[] result = new T[inner.Length];
+
+            int i = 0;
+            for (int j = inner.Length - 1; j >= 0; j--)
+            {
+                result[i] = inner[j];
+                i++;
+            }
+
+            inner = result;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class File1
+    {
+        public string Name { get; set; }
+        public string Extension { get; set; }  // тип
+        public string Size { get; set; }       // размер
+
+        public File1(string name, string extension, string size)
+        {
+            Name = name;
+            Extension = extension;
+            Size = size;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"\t{Name}\n\t\tExtension: {Extension}\n\t\tSize: {Size}");
+        }
+    }
+
+    class Movie : File1
+    {
+        public string Info { get; set; }
+
+        public Movie(string name, string extension, string size, string info)
+    : base(name, extension, size)
+        {
+            Info = info;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            string Resolution = Info.Substring(0, Info.LastIndexOf(';'));
+            string Lenght = Info.Substring(Info.LastIndexOf(';') + 1);
+            Console.WriteLine($"\t\tResolution: {Resolution}\n\t\tLenght: {Lenght}");
+        }
+    }
+
+    class Image : File1
+    {
+        public string Resolution { get; set; }
+
+        public Image(string name, string extension, string size, string res)
+    : base(name, extension, size)
+        {
+            Resolution = res;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine($"\t\tResolution: {Resolution}");
+        }
+    }
+
+    class Text : File1
+    {
+        public string Content { get; set; }
+
+        public Text(string name, string extension, string size, string con)
+    : base(name, extension, size)
+        {
+            Content = con;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine($"\t\tContent: {Content}");
+        }
+    }
+
+    public static class SplitString
+    {
+        public static string Name(string sourceString)
+        {
+            return sourceString.Substring(sourceString.IndexOf(':') + 1,
+                sourceString.IndexOf('(') - sourceString.IndexOf(':') - 1);
+        }
+
+        public static string Extension(string sourceString)
+        {
+            return sourceString.Substring(sourceString.LastIndexOf('.') + 1,
+                sourceString.IndexOf('(') - sourceString.LastIndexOf('.') - 1);
+        }
+
+        public static string Size(string sourceString)
+        {
+            return sourceString.Substring(sourceString.IndexOf('(') + 1,
+                sourceString.IndexOf(')') - sourceString.IndexOf('(') - 1);
+        }
+
+        public static string Info(string sourceString)
+        {
+            return sourceString.Substring(sourceString.IndexOf(';') + 1);
+
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\student\source\repos\KATE\OOPcontrol\ConsoleApp1\ConsoleApp1\file.txt";
+            FileInfo fileInf = new FileInfo(path);
+            string[] allText = null;
+
+            if (fileInf.Exists)
+            {
+                try
+                {   // Чтение файла
+                    // Чтение всех строк файла в массив строк
+                    allText = File.ReadAllLines(path);
+                }
+                catch (FileNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            ConsoleApp1.List<File1> list = new List<File1>(); // список всех объектов
+
+            var sortedText = from stringText in allText
+                             orderby stringText[0] descending
+                             select stringText;
+
+
+            foreach (string s in sortedText)
+            {
+                if (s[0] == 'T')
+                {
+                    Text text = new Text(SplitString.Name(s), SplitString.Extension(s),
+                        SplitString.Size(s), SplitString.Info(s));
+                    list.Add(text);
+                }
+                else if (s[0] == 'I')
+                {
+                    Image images = new Image(SplitString.Name(s), SplitString.Extension(s),
+                        SplitString.Size(s), SplitString.Info(s));
+                    list.Add(images);
+                }
+                else
+                {
+                    Movie movies = new Movie(SplitString.Name(s), SplitString.Extension(s),
+                        SplitString.Size(s), SplitString.Info(s));
+                    list.Add(movies);
+                }
+            }
+
+            Console.WriteLine("Text files:\n");
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].GetType().ToString().Equals("ConsoleApp1.Text"))
+                {
+                    list[i].Display();
+                }
+            }
+
+            Console.WriteLine("Movies:\n");
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].GetType().ToString().Equals("ConsoleApp1.Movie"))
+                {
+                    list[i].Display();
+                }
+            }
+
+            Console.WriteLine("Images:\n");
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].GetType().ToString().Equals("ConsoleApp1.Image"))
+                {
+                    list[i].Display();
+                }
+            }
+        }
+    }
+}
